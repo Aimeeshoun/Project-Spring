90b0db38f92b0d87c6e710ae0a37185d8e6aab09
aimee <10893245@my.uvu.edu>
2021-12-18 10:32:56 -0700

    Trying to get visual studio working, random spawn, load scene, onclick

---
+++ project spring/Assets/scripts/OnClick.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class OnClick : MonoBehaviour
+{
+	public Button yourButton;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(LoadScene);
+	}
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+}
