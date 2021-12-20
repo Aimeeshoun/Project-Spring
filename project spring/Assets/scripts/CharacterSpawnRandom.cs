3d5a60e29b8b1ba75f729f0b7db9afef80a58c0d
aimee <10893245@my.uvu.edu>
2021-12-20 16:29:40 -0700

    Working on making character spawner

---
+++ project spring/Assets/scripts/CharacterSpawnRandom.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSpawnRandom : MonoBehaviour
+
+{
+
+    public GameObject[] myWeaponList2;
+    public GameObject[] spawnPoints;
+    private GameObject currentPoint;
+    private int index;
+    public int spawnAmount;
+    public float spawnRate;
+    private bool spawning;
+
+
+
+
+    public float maxTime = 50;
+    public float minTime = 10;
+    private float spawnTime;
+
+    private float time;
+    private GameObject currentWeapon;
+    private int index2;
+
+
+    public float maxSpawnPerSecond = 2f;
+    private float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+
+        myWeaponList2 = GameObject.FindGameObjectsWithTag("WeaponRandomObj");
+        SetRandomTime();
+        time = 0;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+        time += Time.deltaTime;
+        if (time >= spawnTime)
+        {
+            if (transform.childCount < spawnAmount && spawning == false)
+            {
+
+                if (maxSpawnPerSecond > 0)
+                {
+                    timer -= Time.deltaTime;
+
+                    if (timer <= 0)
+                    {
+                        StartCoroutine(SpawnMonster());
+                        SpawnMonster();
+                        SetRandomTime();
+                        time = 0;
+
+                        timer = 1 / maxSpawnPerSecond;
+                    }
+                }
+
+
+            }
+        }
+    }
+
+    public IEnumerator SpawnMonster()
+    {
+
+        selectRandomObjects();
+        index = Random.Range(0, spawnPoints.Length);
+        currentPoint = spawnPoints[index];
+        currentWeapon = Instantiate(currentWeapon, currentPoint.transform.position, currentPoint.transform.rotation) as GameObject;
+        currentWeapon.transform.parent = gameObject.transform;
+        Destroy(currentWeapon, 3.5f);
+        yield return new WaitForSeconds(spawnRate);
+        spawning = false;
+
+    }
+
+    public void selectRandomObjects()
+
+    {
+        // selectRandomObjects();
+        myWeaponList2 = GameObject.FindGameObjectsWithTag("WeaponRandomObj");
+        index = Random.Range(0, myWeaponList2.Length);
+        currentWeapon = myWeaponList2[index];
+
+
+    }
+
+
+    void SetRandomTime()
+    {
+        spawnTime = Random.Range(minTime, maxTime);
+
+    }
+}
+
+
+
+
+}
