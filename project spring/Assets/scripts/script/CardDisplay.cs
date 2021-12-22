7a2a02961cc3a1f8220687f49447958d943913a4
aimee <10893245@my.uvu.edu>
2021-12-21 17:03:35 -0700

    still working on it

--- project spring/Assets/scripts/script/CardDisplay.cs
+++ project spring/Assets/scripts/script/CardDisplay.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class CardDisplay : MonoBehaviour
 {
    
     
     
     public CollectionSO card;
-    public Text nameText;
     public Image artworkImage;
+    public Text nameText;
+
+  
+   
     void Start()
     {
-        
+        nameText.text = card.name;
+  //     artworkImage.sprite = CollectionSO.artwork;
     }
 
     // Update is called once per frame
--- project spring/Assets/scripts/script/CollectionSO.cs
+++ project spring/Assets/scripts/script/CollectionSO.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 [CreateAssetMenu]
 public class CollectionSO : ScriptableObject
 {
     public bool collected;
     public int WeaponNumber;
     public GameObject art3D;
-    public Sprite art2D;
+    public Image artwork;
+    public Sprite artwork2;
+    public Text nameText;
     public string name;
+    public string tag;
+    
+    
 
 
 }
