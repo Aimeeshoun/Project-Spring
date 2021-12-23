56aef22716c6f13744482e2997cf82f383953bba
aimee <10893245@my.uvu.edu>
2021-12-22 20:30:12 -0700

    trying to get it to work, id communicator

--- project spring/Assets/scripts/script/Collections.cs
+++ project spring/Assets/scripts/script/Collections.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 [CreateAssetMenu]
 public class Collections : ScriptableObject
@@ -13,50 +12,9 @@ public class Collections : ScriptableObject
     public List<WeaponController> WeaponControllerList;
 
 
-    public InputField weaponselectTagInput;
-    public InputField weaponTagInput;
-    public InputField characterselectTagInput;
-    public InputField characterTagInput;
-
-    public bool isSelected;
-
-    private GameObject weapon;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    private void Awake()
-    {
-    
-    }
-
-    public void CompareWeapons(CollectionSO obj)
-    {
-
-        foreach (var CollectionSO in CollectionSOList)
-        {
-            if (weaponTagInput == weaponselectTagInput)
-
-            {
-               
-            }
-
-        }
-
-    }
 
 
 
 
- }
 
-
+}
--- project spring/Assets/scripts/script/IfObject.cs
+++ project spring/Assets/scripts/script/IfObject.cs
@@ -6,28 +6,6 @@ using UnityEngine.UI;
 public class IfObject : MonoBehaviour
 {
 
-    public CollectionSO ColSOcharList;
-    public CollectionSO ColSOweaList;
-    public CollectionSO ColSOcharselList;
-    public CollectionSO ColSOweaselList;
-    
-    public WeaponNUmber WeaNcharList;
-    public WeaponNUmber WeaNweaList;
-    public WeaponNUmber WeaNcharselList;
-    public WeaponNUmber WeaNweaselList;
-    
-    public WeaponController WeaConcharList;
-    public WeaponController WeaConweaList;
-    public WeaponController WeacharselConList;
-    public WeaponController WeaConweaselList;
- 
-
-    public InputField weaponselectTagInput;
-    public InputField weaponTagInput;
-    public InputField characterselectTagInput;
-    public InputField characterTagInput;
-
-    public bool isSelected;
 
     // Start is called before the first frame update
     void Start()
@@ -42,40 +20,6 @@ public class IfObject : MonoBehaviour
     }
 
 
-    public void CompareWeapons()
-    {
-
-        foreach (i in ColSOweaList)
-        {
-            if (weaponTagInput == weaponselectTagInput)
-
-            {
-                isSelected = true;
-            }
-
-        }
-
-    }
-
-    void CompareCharacters(string sc, string c)
-    {
-        if(sc == c)
-
-        { }
-
-
-
-    }
-
-    void ChangeInputTag(string sc, string c)
-    {
-        if (sc == c)
-
-        { }
-
-
-
-    }
 
 
 
