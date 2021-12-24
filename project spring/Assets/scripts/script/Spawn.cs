4a1db902fa0473cea911b17976e612f2677d759f
aimee <10893245@my.uvu.edu>
2021-12-24 15:05:38 -0700

    Got Spawning script to work

--- project spring/Assets/scripts/script/Spawn.cs
+++ project spring/Assets/scripts/script/Spawn.cs
@@ -2,54 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectSpawn : MonoBehaviour
+public class Spawn : MonoBehaviour
 {
 
-
-
     //spawn weapon 
-     
+
     public GameObject[] myWeaponList2;
     public GameObject[] spawnPoints;
     private GameObject currentPoint;
     private int index1;
     public int spawnAmount = 2;
-    public float spawnRate = .5;
+    public float spawnRate = 5;
     private bool spawning;
+    
     public float maxTime = 5;
     public float minTime = 1;
     private float spawnTime;
     private float time;
     private GameObject currentWeapon;
-    
+    private int index5;
     public float maxSpawnPerSecond = 2f;
     private float timer;
 
     //selected weap
 
-    public GameObject[] myWeaponList2;
+
     private GameObject currentWeapon2;
     private int index2;
     public GameObject target;
     private float spawnTime2;
-   
+
 
     //spawn character
 
     public GameObject[] myCharacterList;
-   // public GameObject[] spawnPoints;         //might not need
+    // public GameObject[] spawnPoints;         //might not need
     private GameObject currentPoint2;
     private int index3;
-     private GameObject currentCharacter;
+    private GameObject currentCharacter;
     private float timer2;
-    
+    private int index6;
     //seclected char
-      
-    public GameObject[] myCharacterList;
+
     private GameObject currentCharacter2;
     private int index4;
+   
     public GameObject target2;
- 
+
 
 
     //weapon spawn
@@ -62,13 +61,39 @@ public class ObjectSpawn : MonoBehaviour
 
         //myWeaponList2 = GameObject.FindGameObjectsWithTag("WeaponRandomObj");
         SetRandomTime();
+ 
         time = 0;
+    
+            // selectRandomObjects();
+            // myWeaponList2 = GameObject.FindGameObjectsWithTag("WeaponRandom");
+            index2 = Random.Range(0, myWeaponList2.Length);
+            currentWeapon2 = myWeaponList2[index2];
+            currentWeapon2 = Instantiate(currentWeapon2, target.transform.position, target.transform.rotation) as GameObject;
+            currentWeapon2.transform.parent = gameObject.transform;
+
+
+
+            // character sel
+
+
+   
+
+            
+                // selectRandomObjects();
+                // myWeaponList2 = GameObject.FindGameObjectsWithTag("WeaponRandom");
+                index4 = Random.Range(0, myCharacterList.Length);
+                currentCharacter2 = myCharacterList[index4];
+                currentCharacter2 = Instantiate(currentCharacter2, target2.transform.position, target2.transform.rotation) as GameObject;
+                currentCharacter2.transform.parent = gameObject.transform;
+
+
+
+     
 
 
 
 
-
-    }
+        }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -87,9 +112,13 @@ public class ObjectSpawn : MonoBehaviour
 
                     if (timer <= 0)
                     {
-                        StartCoroutine(SpawnWeapon();
+                        StartCoroutine(SpawnWeapon());
                         SpawnWeapon();
                         SetRandomTime();
+                        StartCoroutine(SpawnCharacter());
+                        SpawnCharacter();
+                        SetRandomTime();
+
                         time = 0;
 
                         timer = 1 / maxSpawnPerSecond;
@@ -99,35 +128,6 @@ public class ObjectSpawn : MonoBehaviour
 
             }
         }
-
-
-        time += Time.deltaTime;
-        if (time >= spawnTime2)
-        {
-            if (transform.childCount < spawnAmount && spawning == false)
-            {
-
-                if (maxSpawnPerSecond > 0)
-                {
-                    timer2 -= Time.deltaTime;
-
-                    if (timer2 <= 0)
-                    {
-                        StartCoroutine(SpawnCharacter());
-                        SpawnCharacter();
-                        SetRandomTime();
-                        time = 0;
-
-                        timer2 = 1 / maxSpawnPerSecond;
-                    }
-                }
-
-
-            }
-        }
-
-
-
 
     }
 
@@ -148,10 +148,10 @@ public class ObjectSpawn : MonoBehaviour
     public void selectRandomWeapons()
 
     {
-        // selectRandomObjects();
-        myWeaponList2 = GameObject.FindGameObjectsWithTag("WeaponRandomObj");
-        index1 = Random.Range(0, myWeaponList2.Length);
-        currentWeapon = myWeaponList2[index1];
+        
+        
+        index5 = Random.Range(0, myWeaponList2.Length);
+        currentWeapon = myWeaponList2[index5];
 
 
     }
@@ -174,7 +174,7 @@ public class ObjectSpawn : MonoBehaviour
     public IEnumerator SpawnCharacter()
     {
 
-        selectRandomCharacter()
+        selectRandomCharacter();
         index3 = Random.Range(0, spawnPoints.Length);
         currentPoint2 = spawnPoints[index3];
         currentCharacter = Instantiate(currentCharacter, currentPoint.transform.position, currentPoint2.transform.rotation) as GameObject;
@@ -188,78 +188,24 @@ public class ObjectSpawn : MonoBehaviour
     public void selectRandomCharacter()
 
     {
-        // selectRandomObjects();
-        myCharacterList = GameObject.FindGameObjectsWithTag("CharacterRandomObj");
-        index3 = Random.Range(0, myCharacterList.Length);
-        currentCharacter = myCharacterList[index3];
-
-    }
-
-
-    void SetRandomTime()
-    {
-        spawnTime = Random.Range(minTime, maxTime);
+  
+     
+        index6 = Random.Range(0, myCharacterList.Length);
+        currentCharacter = myCharacterList[index6];
 
     }
 
 
 
-    //weapon sel
-
-    public void Start()
-
-    {
-        // selectRandomObjects();
-        // myWeaponList2 = GameObject.FindGameObjectsWithTag("WeaponRandom");
-        index2 = Random.Range(0, myWeaponList2.Length);
-        currentWeapon2 = myWeaponList2[index2];
-        currentWeapon2 = Instantiate(currentWeapon2, target.transform.position, target.transform.rotation) as GameObject;
-        currentWeapon2.transform.parent = gameObject.transform;
-      
 
 
-        // character sel
 
-
-    public void Start()
-
-    {
-        // selectRandomObjects();
-        // myWeaponList2 = GameObject.FindGameObjectsWithTag("WeaponRandom");
-        index4 = Random.Range(0, myCharacterList.Length);
-            currentCharacter2 = myCharacterList[index4];
-            currentCharacter2 = Instantiate(currentCharacter2, target2.transform.position, target2.transform.rotation) as GameObject;
-            currentCharacter2.transform.parent = gameObject.transform;
-       
 
 
     }
 
 
 
-}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
