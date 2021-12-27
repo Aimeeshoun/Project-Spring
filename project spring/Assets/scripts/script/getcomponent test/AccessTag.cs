41c412ba94674a1e55b1a8e50c651a97cb497b6e
aimee <10893245@my.uvu.edu>
2021-12-27 12:36:05 -0700

    Get component test

--- project spring/Assets/scripts/script/getcomponent test/AccessTag.cs
+++ project spring/Assets/scripts/script/getcomponent test/AccessTag.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class AccessTag : MonoBehaviour
 {
 
+    
     public IDholder _otherdholder;
+    
     public IDholder _thisidholder;
+    public IDholder _selWepIdholder;
+    public IDholder _selCharIdholder;
+
     public GameObject otherGameObject;
     public GameObject thisGameObject;
 
@@ -16,41 +21,43 @@ public class AccessTag : MonoBehaviour
     public CharacterSpawnRandom _characterSpawnRandom;
     public SpawnRandomPoints _spawnRandomPoints;
 
-    private GameObject _selWeap;
-    private GameObject _spawnWeap; 
-    private GameObject _spawnChar; 
-    private GameObject _selChar;
+    public GameObject _selWeap;
+    public GameObject _spawnWeap;
+    public GameObject _spawnChar;
+    public GameObject _selChar;
 
-    private IDholder _selWeapId;
-    private IDholder _spawnWeapId;
-    private IDholder _spawnCharId;
-    private IDholder _selCharId;
+    public IDholder _selWeapId;
+    public IDholder _spawnWeapId;
+    public IDholder _spawnCharId;
+    public IDholder _selCharId;
 
-    private GameObject _selWeapIdObj;
-    private GameObject _spawnWeapIdObj;
-    private GameObject _spawnCharIdObj;
-    private GameObject _selCharIdObj;
-
-
-    private GameObject _selWeapIdObjGO;
-    private GameObject _spawnWeapIdObjGO;
-    private GameObject _spawnCharIdObjGO;
-    private GameObject _selCharIdObjGO;
+    public GameObject _selWeapIdObj;
+    public GameObject _spawnWeapIdObj;
+    public GameObject _spawnCharIdObj;
+    public GameObject _selCharIdObj;
 
 
+    public GameObject _selWeapIdObjGO;
+    public GameObject _spawnWeapIdObjGO;
+    public GameObject _spawnCharIdObjGO;
+    public GameObject _selCharIdObjGO;
 
 
+    public string _selWeapIdObjName;
+    public string _spawnWeapIdObjName;
+    public string _spawnCharIdObjName;
+    public string _selCharIdObjName;
 
-    private string _selWeapIdObjName;
-    private string _spawnWeapIdObjName;
-    private string _spawnCharIdObjName;
-    private string _selCharIdObjName;
+    public string _thisObjName;
+    public string _otherObjName;
 
-    private string _thisObjName;
-    private string _otherObjName;
+    public ID thisidtag;
+    public ID otheridtag;
+    public ID selwepidtag;
+    public ID selcharidtag;
 
-    private string _currentselWeapname;
-    private string _currentselCharname;
+    //   private string _currentselWeapname;
+    // private string _currentselCharname;
 
 
     void Start()
@@ -65,12 +72,16 @@ public class AccessTag : MonoBehaviour
     private void Awake()
     {
         _thisidholder = this.gameObject.GetComponent<IDholder>();
+        thisidtag = _thisidholder.idObj;
+        selwepidtag = _selWepIdholder.idObj;
+        selcharidtag = _selCharIdholder.idObj;
 
-        _selectedChar = otherGameObject.GetComponent<SelectedChar>();
-        _selectedObjects = otherGameObject.GetComponent<SelectedObjects>();
 
-        _characterSpawnRandom = otherGameObject.GetComponent<CharacterSpawnRandom>();
-        _spawnRandomPoints = otherGameObject.GetComponent<SpawnRandomPoints>();
+        _selectedChar = _selectedChar.GetComponent<SelectedChar>();
+        _selectedObjects = _selectedObjects.GetComponent<SelectedObjects>();
+
+        _characterSpawnRandom = _characterSpawnRandom.GetComponent<CharacterSpawnRandom>();
+        _spawnRandomPoints = _spawnRandomPoints.GetComponent<SpawnRandomPoints>();
 
         _selWeap = _selectedObjects.currentWeapon;
         _spawnWeap = _spawnRandomPoints.currentWeapon;
@@ -81,46 +92,46 @@ public class AccessTag : MonoBehaviour
         _spawnWeapId = _spawnWeap.GetComponent<IDholder>();
         _spawnCharId = _spawnChar.GetComponent<IDholder>();
         _selCharId = _selChar.GetComponent<IDholder>();
-
+        Destroy(_thisidholder.idObj);
         _selWeapIdObj = _selWeapId.GetComponent<GameObject>() as GameObject;
         _spawnWeapIdObj = _spawnWeapId.GetComponent<GameObject>() as GameObject;
         _spawnCharIdObj = _spawnCharId.GetComponent<GameObject>() as GameObject;
         _selCharIdObj = _selCharId.GetComponent<GameObject>() as GameObject;
 
-        _selWeapIdObjName = _selWeapIdObj.name;
-        _spawnWeapIdObjName  = _spawnWeapIdObj.name;
-        _spawnCharIdObjName  = _spawnCharIdObj.name;
-        _selCharIdObjName  = _selCharIdObj.name;
+        _selWeapIdObjName = _selWeapId.name;
+        _spawnWeapIdObjName  = _spawnWeapId.name;
+        _spawnCharIdObjName  = _spawnCharId.name;
+        _selCharIdObjName  = _selCharId.name;
 
         thisGameObject = this.gameObject;
-        _thisObjName = thisGameObject.name;
+    
         
 
-        _currentselWeapname = _selWeap.name;
-        _currentselCharname= _selChar.name;
+       // _currentselWeapname = _selWeap.name;
+      // _currentselCharname= _selChar.name;
 
 
     }   
     public void OntriggerEnter(Collider other)
 
     {
-        
+
 
         _otherdholder = other.gameObject.GetComponent<IDholder>();
-
+        otheridtag = _otherdholder.idObj;
         otherGameObject = other.gameObject;
-        _otherObjName = otherGameObject.name;
+      
         
 
-        if (_currentselWeapname == _thisObjName)
+        if (thisidtag == selwepidtag)
         {
-            if (_otherObjName == _currentselCharname)
+            if (otheridtag == selcharidtag)
             {
                
                    Destroy(_selWeap);
                    Destroy(_selChar);
-                   Destroy(thisGameObject);
-                   Destroy(otherGameObject);
+                   Destroy(this.gameObject);
+                   Destroy(other.gameObject);
 
             }
         }
