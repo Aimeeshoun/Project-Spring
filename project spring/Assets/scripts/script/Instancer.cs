9856d18827d3ade7ea4408d4a3e53dd64c075ef2
aimee <10893245@my.uvu.edu>
2022-01-06 13:07:35 -0700

    gun works

--- project spring/Assets/scripts/script/Instancer.cs
+++ project spring/Assets/scripts/script/Instancer.cs
@@ -3,36 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Instancer : MonoBehaviour
-{ 
- 
+{
+
     public GameObject bullet;
- 
-
-
+    public GameObject gun;
+    public Transform bulletVelocity;
+    private bool spawning;
+    public float spawnRate;
     public float BulletSpeed;
-  
     public AudioClip GunshotSound;
+    public GameObject newbullet;
+
+    
 
 
-    // Use this for initialization
-    void Start()
-{
-
-}
-
-// Update is called once per frame
-void Update()
-{
- 
-        Fire();
+    public void Fire()
+    {
 
 
 
-}
+        newbullet = Instantiate(bullet, gun.transform.position, gun.transform.rotation) as  GameObject;
 
-void Fire()
-{
-        bullet = Instantiate(bullet, this.gameObject.transform.position, this.gameObject.transform.rotation);
-        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * BulletSpeed);
     }
+
 }
+
+
