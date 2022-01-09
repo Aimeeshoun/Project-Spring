b2f94fe2d04067da5631b9070c892140b6f76e26
aimee <10893245@my.uvu.edu>
2022-01-09 12:10:56 -0700

    button movement system

--- project spring/Assets/scripts/button scripts/directions.cs
+++ project spring/Assets/scripts/button scripts/directions.cs
@@ -9,32 +9,65 @@ public class directions : MonoBehaviour
     private Vector3 thePosition;
 
     private Vector3 currentRotation;
+
+    public GameObject target;
+
+
+
+    public bool left1;
+    public bool right1;
+    public bool up;
+    public bool down;
+
     public void Start()
     {
 
         m_Rigidbody = GetComponent<Rigidbody>();
-
+     
         m_Speed = 30.0f;
+
+       
     }
 
     public void Update()
     {
+   
+
+   //     if (right1 || left1)
+   //     {
+     //       m_Rigidbody.rotation = Quaternion.Euler(0, 0.0f, 0);
+
+     //   }
+     
+
+   // if (up || down)
+   //     {
+
+   //         m_Rigidbody.rotation = Quaternion.Euler(0, 90.0f, 0);
+   //     }
+
+
+
     }
 
-    public void MoveForward()
+    public void right()
     {
         //  m_Rigidbody.velocity = -transform.up * m_Speed;
         // transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * m_Speed, Space.World);
-
-
-        m_Rigidbody.rotation = Quaternion.Euler(0, 0.0f, 0);
-        m_Rigidbody.velocity = -transform.right * m_Speed;
+        right1 = true;
+    
+        //  m_Rigidbody.rotation = Quaternion.Euler(0, 0.0f, 0);
+        m_Rigidbody.velocity = transform.right * m_Speed;
+  
     }
 
-    public void GoBack()
+    public void left()
     {
-        m_Rigidbody.rotation = Quaternion.Euler(0, 0f, 0);
-        m_Rigidbody.velocity = transform.right * m_Speed;
+        left1 = true;
+
+        //   m_Rigidbody.rotation = Quaternion.Euler(0, 90f, 0);
+        //  m_Rigidbody.velocity = -transform.right * m_Speed;
+        m_Rigidbody.velocity = -transform.right * m_Speed;
 
     }
 
@@ -44,21 +77,25 @@ public class directions : MonoBehaviour
     public void MoveUp()
     {
 
-
+        up = true;
         //   m_Rigidbody.velocity = transform.up * m_Speed;
         //  transform.Rotate(new Vector3(0,-1, 0) * Time.deltaTime * m_Speed, Space.World);
-        m_Rigidbody.rotation =  Quaternion.Euler(0, 90.0f, 0);
-        m_Rigidbody.velocity = transform.right * m_Speed;
+
+        m_Rigidbody.rotation = Quaternion.Euler(0, 90.0f, 0);
+    //    m_Rigidbody.velocity = transform.right * m_Speed;
+       
     }
 
     public void MoveDown()
     {
 
-
+        down= true;
         //   m_Rigidbody.velocity = transform.up * m_Speed;
         //  transform.Rotate(new Vector3(0,-1, 0) * Time.deltaTime * m_Speed, Space.World);
-        m_Rigidbody.rotation = Quaternion.Euler(0, 90.0f, 0);
-        m_Rigidbody.velocity = -transform.right * m_Speed;
+        m_Rigidbody.rotation = Quaternion.Euler(0, 0f, 0);
+
+     //  m_Rigidbody.velocity = -transform.right * m_Speed;
+  
     }
 
 
