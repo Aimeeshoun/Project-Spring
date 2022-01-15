439bb1b9873f7dbc5230d571fdc52bc8cf24e5d0
aimee <10893245@my.uvu.edu>
2022-01-15 14:11:04 -0700

    cow death working

--- project spring/Assets/scripts/InstanciteDeadCow2.cs
+++ project spring/Assets/scripts/InstanciteDeadCow2.cs
@@ -8,35 +8,40 @@ public class InstanciteDeadCow2 : MonoBehaviour
     public GameObject deadcow;
     public GameObject deadcow2;
     public GameObject this_gamObj;
+    public GameObject this_gamObj2;
 
-    private int index;
     private bool spawning;
     private bool spawning2;
     public float spawnRate;
-
-
-
+    public Transform position_;
+    public Vector3 _postion;
+    public Quaternion _rotation;
 
     public void Start()
     {
-        this_gamObj = this.gameObject;
-        this_gamObj = this_gamObj.GetComponentInChildren<GameObject>();
+        //   this_gamObj = this.gameObject;
+        //  this_gamObj = this_gamObj.GetComponentInChildren<GameObject>();
+       
+    //    this_gamObj2= this_gamObj.GetComponentInChildren<GameObject>()as GameObject;
     }
     // Update is called once per frame
     public void Update()
     {
-   
         
+        position_ = this_gamObj2.GetComponent<Transform>();
+        _postion = position_.position;
+        _rotation = position_.rotation;
+
     }
 
     public void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Fuzzy")
         {
-            this_gamObj.SetActive(false);
+            
             StartCoroutine(Killcow());
             Killcow();
-          //  this_gamObj.SetActive(false);
+            this_gamObj.SetActive(false);
         }
     }
    
@@ -46,9 +51,9 @@ public class InstanciteDeadCow2 : MonoBehaviour
 
     public IEnumerator Killcow()
     {
-        deadcow = Instantiate(deadcow, this_gamObj.transform.position, this_gamObj.transform.rotation) as GameObject;
+        deadcow = Instantiate(deadcow, _postion, _rotation) as GameObject;
         deadcow.transform.parent = gameObject.transform;
-        Destroy(deadcow, 7f);
+        Destroy(deadcow, 4f);
         yield return new WaitForSeconds(spawnRate);
         spawning = false;
         StartCoroutine(Killcow2());
@@ -59,7 +64,7 @@ public class InstanciteDeadCow2 : MonoBehaviour
     {
         deadcow2 = Instantiate(deadcow2, this_gamObj.transform.position, this_gamObj.transform.rotation) as GameObject;
         deadcow2.transform.parent = gameObject.transform;
-        Destroy(deadcow2, 14f);
+        Destroy(deadcow2, 4f);
         yield return new WaitForSeconds(spawnRate);
         spawning2 = false;
 
