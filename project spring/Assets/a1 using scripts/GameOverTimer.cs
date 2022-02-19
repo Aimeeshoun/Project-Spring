3f37a9e15483d05f942096d3056f19c6971224a5
aimee <10893245@my.uvu.edu>
2022-02-19 14:29:05 -0700

    timer to game over works

---
+++ project spring/Assets/a1 using scripts/GameOverTimer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverTimer : MonoBehaviour
+{
+
+    public int Time;
+    public int newTimer = 0;
+    public int current_time;
+    public GameObject time_obj;
+    public TIMER timer_;
+    public int time_int;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time_obj= GameObject.FindGameObjectWithTag("TIMER2");
+        timer_ = time_obj.GetComponent<TIMER>();
+        
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time_int = timer_.current_time;
+
+        if (time_int == 200000)
+        {
+            SceneManager.LoadScene("game over menu");
+        }
+    }
+}
+
+
