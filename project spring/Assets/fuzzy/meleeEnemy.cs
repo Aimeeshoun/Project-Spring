09e5262a58ec09d00f89b34874463de5cb0611d8
aimee <10893245@my.uvu.edu>
2022-03-03 16:01:40 -0700

    particle system kinda works

--- project spring/Assets/fuzzy/meleeEnemy.cs
+++ project spring/Assets/fuzzy/meleeEnemy.cs
@@ -7,27 +7,50 @@ public class meleeEnemy : MonoBehaviour
     public GameObject oth_gameObject;
     public KillEnemy killScript;
 
+    public GameObject particlesystem;
+    public ParticleSystem particleSystem_;
+    public GameObject bloodPar_;
 
+    public Transform transform_;
+    public Quaternion other_obj_rotation;
+    public Vector3 this_obj_;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        particlesystem = GameObject.FindGameObjectWithTag("particle blood");
+        particleSystem_ = particlesystem.GetComponent<ParticleSystem>();
     }
 
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-
         oth_gameObject = other.gameObject;
-        if (oth_gameObject.tag == ("Fuzzy"))
-            {
 
-             }
+        if (oth_gameObject.tag == ("Fuzzy"))
+        {
+            transform_ = oth_gameObject.transform;
+            other_obj_rotation = transform_.rotation;
+            this_obj_ = transform_.position;
+            bloodPar_ = Instantiate(particlesystem, this_obj_, other_obj_rotation);
+            bloodPar_.transform.parent = gameObject.transform;
+        }
+
+
+
+
     }
+
 }
+
+
+
+ 
+      
