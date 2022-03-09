74b1cd19d69e7a14d5e1a70358aabaaa320a6ebf
aimee <10893245@my.uvu.edu>
2022-03-08 23:07:22 -0700

    trying to get fire to work

--- project spring/Assets/fuzzy/CandleFire.cs
+++ project spring/Assets/fuzzy/CandleFire.cs
@@ -14,6 +14,9 @@ public class CandleFire : MonoBehaviour
     public Vector3 fuzzy_Vec;
     public Quaternion fuzzy_Rot;
 
+    public bool isOnFire;
+    public bool isdead;
+
 
     public GameObject thefire;
     // Start is called before the first frame update
@@ -37,11 +40,63 @@ public class CandleFire : MonoBehaviour
 
 
         {
-            fuzzy_trans= other_candlebear.transform;
-            fuzzy_Vec= fuzzy_trans.position;
+            fuzzy_trans = other_candlebear.transform;
+            fuzzy_Vec = fuzzy_trans.position;
             fuzzy_Rot = fuzzy_trans.rotation;
 
-            firestarter2 = Instantiate(thefire, fuzzy_Vec, fuzzy_Rot) as GameObject;
+            StartCoroutine(FireStarter());
+
+            FireStarter();
+
+            if (isOnFire)
+            {
+                FireStarter2();
+                FireStarter3();
+            }
         }
+
+
+
+    }
+
+    public IEnumerator FireStarter()
+    {
+
+
+        isOnFire = true;
+
+        yield return new WaitForSeconds(.5f);
+        isdead = true;
+        isOnFire = false;
+      
+
+
+
+    }
+
+
+
+    public void FireStarter2()
+    {
+
+  
+        firestarter2 = Instantiate(thefire, fuzzy_Vec, fuzzy_Rot) as GameObject;
+
+    
+
+     
+
+    }
+
+    public void FireStarter3()
+    {
+
+
+        Destroy(other_candlebear, .5f);
+        Destroy(firestarter2, .5f);
+
+     
+
+
     }
 }
